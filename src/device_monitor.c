79612550bdeaca27cc64717c1b13b468e70a11dd
Jeija <norrepli@gmail.com>
2019-10-30 07:52:24 +0100

    Device monitor: Some coding style-related changes

--- src/device_monitor.c
+++ src/device_monitor.c
@@ -1,15 +1,15 @@
 /***********************************************************************/
-/* device_mintor.h                                                     */
-/* ---------                                                           */
-/*           GTKTerm Software                                          */
-/*                      (c) Julien Schmitt                             */
-/*                                                                     */
+/* device_mintor.h													 */
+/* ---------														   */
+/*		   GTKTerm Software										  */
+/*					  (c) Julien Schmitt							 */
+/*																	 */
 /* ------------------------------------------------------------------- */
-/*                                                                     */
-/*   Purpose                                                           */
-/*      Monitor device to autoreconnect                                */
-/*   Written by Kevin Picot - picotk27@gmail.com                       */
-/*                                                                     */
+/*																	 */
+/*   Purpose														   */
+/*	  Monitor device to autoreconnect								*/
+/*   Written by Kevin Picot - picotk27@gmail.com					   */
+/*																	 */
 /***********************************************************************/
 
 #include <device_monitor.h>
@@ -25,7 +25,6 @@
 #include <glib.h>
 #include <serial.h>
 #include <interface.h>
-#include <glib/gprintf.h>
 #include <term_config.h>
 #include <gudev/gudev.h>
 
@@ -33,57 +32,52 @@
 
 extern struct configuration_port config;
 
-static inline void device_monitor_status(const bool connected) {
-
-    if( connected )
-        interface_open_port();
-    else
-        interface_close_port();
+static inline void device_monitor_status(const bool connected)
+{
+	if (connected)
+		interface_open_port();
+	else
+		interface_close_port();
 }
 
-static inline void device_monitor_handle(const char *action) {
-
-    if( strcmp(action, "remove") == 0 ) {
-        device_monitor_status(false);
-    } else if( strcmp(action, "add") == 0 ) {
-        device_monitor_status(true);
-    }
+static inline void device_monitor_handle(const char *action)
+{
+	if (strcmp(action, "remove") == 0)
+		device_monitor_status(false);
+	else if (strcmp(action, "add") == 0)
+		device_monitor_status(true);
 }
 
-void event_udev(GUdevClient *client, const gchar *action, GUdevDevice *device) {
+void event_udev(GUdevClient *client, const gchar *action, GUdevDevice *device)
+{
 
-    if( !device || !action )
-        return;
+	if (!device || !action)
+		return;
 
-    if( !g_udev_device_get_device_file(device) )
-        return;
+	if (!g_udev_device_get_device_file(device))
+		return;
 
-    const gchar *name = config.port;
+	const gchar *name = config.port;
 
-    if( strcmp(g_udev_device_get_device_file(device), name) == 0 ) {
-        g_printf("Device %s: %s\n",
-                    g_udev_device_get_device_file(device),
-                    action);
-        device_monitor_handle(action);
-    }
+	if (strcmp(g_udev_device_get_device_file(device), name) == 0)
+		device_monitor_handle(action);
 }
 
-extern void device_monitor_start(void) {
+extern void device_monitor_start(void)
+{
 
-    const gchar *const subsystems[] = {NULL, NULL};
+	const gchar *const subsystems[] = {NULL, NULL};
 
-    /* Initial check */
-    GUdevClient *udev_client = g_udev_client_new(subsystems);
+	/* Initial check */
+	GUdevClient *udev_client = g_udev_client_new(subsystems);
 
-    if( g_udev_client_query_by_device_file(udev_client, config.port) == NULL ) {
-        g_printf("Device %s not detected\n", config.port);
-        device_monitor_status(false);
-    } else {
-        g_printf("Device %s detected\n", config.port);
-        device_monitor_status(true);
-    }
+	if (g_udev_client_query_by_device_file(udev_client, config.port) == NULL) {
+		device_monitor_status(false);
+	} else {
+		device_monitor_status(true);
+	}
 
-    /* Monitor device */
-    g_signal_connect(G_OBJECT(udev_client), "uevent",
-            G_CALLBACK(event_udev), NULL);
+	/* Monitor device */
+	g_signal_connect(G_OBJECT(udev_client), "uevent",
+	                 G_CALLBACK(event_udev), NULL);
 }
