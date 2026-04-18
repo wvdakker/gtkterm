7a32b754b8295ea567992002bed27f1305d0ff95
Boris Krasnovskiy <borkra@gmail.com>
2026-04-17 21:08:29 -0400

    Replace complex list view with simpler list box in config selector

--- src/config_dialog.c
+++ src/config_dialog.c
@@ -9,21 +9,9 @@
 #include "macros.h"
 #include "config_dialog.h"
 
-static void load_config(gpointer, gint, GtkSingleSelection *);
-static void delete_config(gpointer, gint, GtkSingleSelection *);
-static void save_config(gpointer, gint, GtkWidget *);
-
-static void on_load_ok_clicked(GtkSingleSelection *sel, GtkButton *btn)
-{
-	(void)btn;
-	load_config(NULL, GTK_RESPONSE_ACCEPT, sel);
-}
-
-static void on_delete_ok_clicked(GtkSingleSelection *sel, GtkButton *btn)
-{
-	(void)btn;
-	delete_config(NULL, GTK_RESPONSE_ACCEPT, sel);
-}
+static void on_load_ok_clicked(GtkListBox *, GtkButton *);
+static void on_delete_ok_clicked(GtkListBox *, GtkButton *);
+static void on_save_ok_clicked(GtkEditable *, GtkButton *);
 
 static void Select_config(gchar *title, GCallback on_ok)
 {
@@ -31,11 +19,8 @@ static void Select_config(gchar *title, GCallback on_ok)
 	gchar **g;
 	GtkBuilderCScope *scope;
 	GtkBuilder *builder;
-	GtkWidget *dialog;
-	GtkWidget *Liste;
-	GtkStringList *string_list;
-
-	groups = config_get_sections();
+	GtkWindow *dialog;
+	GtkListBox *listbox;
 
 	scope = GTK_BUILDER_CSCOPE(gtk_builder_cscope_new());
 	gtk_builder_cscope_add_callback_symbols(scope,
@@ -49,28 +34,19 @@ static void Select_config(gchar *title, GCallback on_ok)
 	g_object_unref(scope);
 	gtk_builder_add_from_resource(builder, "/org/gtk/gtkterm/select_config_dialog.ui", NULL);
 
-	dialog = GTK_WIDGET(gtk_builder_get_object(builder, "select_config_window"));
-	Liste  = GTK_WIDGET(gtk_builder_get_object(builder, "select_config_list"));
+	dialog  = GTK_WINDOW(gtk_builder_get_object(builder, "select_config_window"));
+	listbox = GTK_LIST_BOX(gtk_builder_get_object(builder, "select_config_list"));
 	g_object_unref(builder);
 
-	gtk_window_set_title(GTK_WINDOW(dialog), title);
-	gtk_window_set_transient_for(GTK_WINDOW(dialog), GTK_WINDOW(Fenetre));
+	gtk_window_set_title(dialog, title);
+	gtk_window_set_transient_for(dialog, GTK_WINDOW(Fenetre));
 
-	string_list = gtk_string_list_new(NULL);
+	groups = config_get_sections();
 	for (g = groups; *g; g++)
-		gtk_string_list_append(string_list, *g);
+		gtk_list_box_append(listbox, gtk_label_new(*g));
 	g_strfreev(groups);
-	gtk_single_selection_set_model(
-	    GTK_SINGLE_SELECTION(gtk_list_view_get_model(GTK_LIST_VIEW(Liste))),
-	    G_LIST_MODEL(string_list));
-	g_object_unref(string_list);
 
-	gtk_window_present(GTK_WINDOW(dialog));
-}
-
-static void on_save_ok_clicked(GtkEditable *entry, GtkButton *btn)
-{
-	save_config(NULL, GTK_RESPONSE_ACCEPT, GTK_WIDGET(entry));
+	gtk_window_present(dialog);
 }
 
 static void check_text_input_alnum(GtkEditable *editable, gchar *new_text,
@@ -84,7 +60,7 @@ static void Save_config_file(void)
 {
 	GtkBuilderCScope *scope;
 	GtkBuilder *builder;
-	GtkWidget *dialog;
+	GtkWindow *dialog;
 
 	scope = GTK_BUILDER_CSCOPE(gtk_builder_cscope_new());
 	gtk_builder_cscope_add_callback_symbols(scope,
@@ -98,106 +74,87 @@ static void Save_config_file(void)
 	g_object_unref(scope);
 	gtk_builder_add_from_resource(builder, "/org/gtk/gtkterm/save_config_dialog.ui", NULL);
 
-	dialog = GTK_WIDGET(gtk_builder_get_object(builder, "save_config_window"));
-	gtk_window_set_transient_for(GTK_WINDOW(dialog), GTK_WINDOW(Fenetre));
-	gtk_window_present(GTK_WINDOW(dialog));
+	dialog = GTK_WINDOW(gtk_builder_get_object(builder, "save_config_window"));
+	gtk_window_set_transient_for(dialog, GTK_WINDOW(Fenetre));
+	gtk_window_present(dialog);
 }
 
-static void really_save_config(gpointer _unused, gint id, gconstpointer data)
+static void really_save_config(const gchar *data)
 {
-	if(id == GTK_RESPONSE_ACCEPT)
-	{
-		if (!Save_configuration_to_file((const gchar *)data))
-			return;
-		show_messagef(MSG_WRN, _("Configuration [%s] saved\n"), (const gchar *)data);
-	}
-	else
-		Save_config_file();
+	if (!Save_configuration_to_file(data))
+		return;
+	show_messagef(MSG_WRN, _("Configuration [%s] saved\n"), data);
 }
 
 static void on_overwrite_response(GObject *source, GAsyncResult *result, gpointer data)
 {
-	GtkAlertDialog *alert = GTK_ALERT_DIALOG(source);
-	GError *error = NULL;
-	int response = gtk_alert_dialog_choose_finish(alert, result, &error);
-	if (!error && response == 0)
-		really_save_config(NULL, GTK_RESPONSE_ACCEPT, data);
-	g_clear_error(&error);
+	int response = gtk_alert_dialog_choose_finish(GTK_ALERT_DIALOG(source), result, NULL);
+	if (response == 0)
+		really_save_config(data);
 	g_free(data);
 }
 
-static void save_config(gpointer _unused, gint id, GtkWidget *edit)
+static void on_save_ok_clicked(GtkEditable *entry, GtkButton *btn)
 {
-	if(id == GTK_RESPONSE_ACCEPT)
+	(void)btn;
+
+	static const char * const buttons[] = { "_Yes", "_Cancel", NULL };
+
+	const gchar *config_name = gtk_editable_get_text(GTK_EDITABLE(entry));
+
+	if (config_section_exists(config_name))
 	{
-		const gchar *config_name;
-		g_autofree gchar *alert_msg;
-		GtkAlertDialog *alert;
-		gboolean has_group;
-		static const char * const buttons[] = { "_Yes", "_Cancel", NULL };
+		g_autofree gchar *alert_msg = g_strdup_printf(
+		    _("Section [%s] already exists.\n\nDo you want to overwrite it ?"),
+		    config_name);
 
-		config_name = gtk_editable_get_text(GTK_EDITABLE(edit));
-		has_group = config_section_exists(config_name);
+		GtkAlertDialog *alert = gtk_alert_dialog_new("%s", alert_msg);
 
-		if (has_group)
+		gtk_alert_dialog_set_modal(alert, TRUE);
+		gtk_alert_dialog_set_buttons(alert, buttons);
+		gtk_alert_dialog_set_default_button(alert, 0);
+		gtk_alert_dialog_set_cancel_button(alert, 1);
+
+		gtk_alert_dialog_choose(alert, GTK_WINDOW(Fenetre), NULL,
+		                        on_overwrite_response, g_strdup(config_name));
+		g_object_unref(alert);
+	}
+	else
+		really_save_config(config_name);
+}
+
+static void on_load_ok_clicked(GtkListBox *listbox, GtkButton *btn)
+{
+	(void)btn;
+
+	GtkListBoxRow *row = gtk_list_box_get_selected_row(listbox);
+	if (row)
+	{
+		const gchar *txt = gtk_label_get_text(GTK_LABEL(gtk_list_box_row_get_child(row)));
+		if (Load_configuration_from_file(txt) == -1)
 		{
-			alert_msg = g_strdup_printf(
-			    _("Section [%s] already exists.\n\nDo you want to overwrite it ?"),
-			    config_name);
-			alert = gtk_alert_dialog_new("%s", alert_msg);
-			gtk_alert_dialog_set_modal(alert, TRUE);
-			gtk_alert_dialog_set_buttons(alert, buttons);
-			gtk_alert_dialog_set_default_button(alert, 0);
-			gtk_alert_dialog_set_cancel_button(alert, 1);
-
-			gtk_alert_dialog_choose(alert, GTK_WINDOW(Fenetre), NULL,
-			                        on_overwrite_response, g_strdup(config_name));
-			g_object_unref(alert);
+			show_messagef(MSG_ERR, _("No section \"%s\" in configuration file\n"), txt);
+			return;
 		}
-		else
-			really_save_config(NULL, GTK_RESPONSE_ACCEPT, (gconstpointer)config_name);
+
+		Verify_configuration();
+		interface_apply_term_config();
+		Config_port();
+		ConfigFlags();
+		update_port_status();
 	}
 }
 
-static void load_config(gpointer _unused, gint id, GtkSingleSelection *selection)
+static void on_delete_ok_clicked(GtkListBox *listbox, GtkButton *btn)
 {
-	gchar *message;
+	(void)btn;
 
-	if(id == GTK_RESPONSE_ACCEPT)
+	GtkListBoxRow *row = gtk_list_box_get_selected_row(listbox);
+	if (row)
 	{
-		GtkStringObject *obj = GTK_STRING_OBJECT(gtk_single_selection_get_selected_item(selection));
-		if(obj)
-		{
-			const gchar *txt = gtk_string_object_get_string(obj);
-			if (Load_configuration_from_file(txt) == -1)
-			{
-				show_messagef(MSG_ERR, _("No section \"%s\" in configuration file\n"), txt);
-				return;
-			}
-			Verify_configuration();
-			interface_apply_term_config();
-			Config_port();
-			ConfigFlags();
-
-			message = get_port_string();
-			Set_status_message(message);
-			Set_window_title(message);
-			g_free(message);
-		}
-	}
-}
-
-static void delete_config(gpointer _unused, gint id, GtkSingleSelection *selection)
-{
-	if(id == GTK_RESPONSE_ACCEPT)
-	{
-		GtkStringObject *obj = GTK_STRING_OBJECT(gtk_single_selection_get_selected_item(selection));
-		if(obj)
-		{
-			const gchar *txt = gtk_string_object_get_string(obj);
-			if (!config_delete_section(txt))
-				show_message(MSG_ERR, _("Cannot delete section!"));
-		}
+		const gchar *txt = gtk_label_get_text(GTK_LABEL(gtk_list_box_row_get_child(row)));
+		if (!config_delete_section(txt))
+			show_message(MSG_ERR, _("Cannot delete section!"));
 	}
 }
 
